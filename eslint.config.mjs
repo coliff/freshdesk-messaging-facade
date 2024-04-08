6d4c8a93891ea585c9b243e5072b82f973d5dcf7
coliff <christianoliff@pm.me>
2024-04-08 19:38:02 +0900

    Update to Eslint v9

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import js from "@eslint/js";
+import globals from "globals";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+  {
+    files: ["**/*.js"],
+    ignores: ["*.min.js"],
+    languageOptions: {
+      ecmaVersion: 2015,
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        myCustomGlobal: "readonly",
+      },
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+    },
+  },
+  eslintConfigPrettier,
+];
